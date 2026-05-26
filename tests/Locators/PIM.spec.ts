fab4f39624666a4bde3b536f98960231adbefe70
bhargav-github <bargav.m123@gmail.com>
2026-05-27 05:26:55 +0530

    update  the log statment

--- tests/Locators/PIM.spec.ts
+++ tests/Locators/PIM.spec.ts
@@ -2,14 +2,17 @@ import{Page} from '@playwright/test'
 
 export async function navigate_PIM(page:Page)
 {
-    if(!await page.locator("div.oxd-brand-banner").isVisible())
-    {
+    // await page.pause();
+    try{
+        await page.locator("//i[@class='oxd-icon bi-chevron-left']").isVisible()
         await page.locator("//button[contains(@class,'oxd-main-menu-button')]").click();
-        const banner = page.locator("div.oxd-brand-banner");
-        await banner.waitFor({
-            state : 'visible',
-            timeout : 30000
-        });
+        await page.waitForSelector("//i[@class='oxd-icon bi-chevron-left']");
+    }catch{
+        if(await page.locator("//i[@class='oxd-icon bi-chevron-left']").isVisible())
+        {
+            await page.locator("//button[contains(@class,'oxd-main-menu-button')]").click();
+            await page.waitForSelector("//i[@class='oxd-icon bi-chevron-left']");
+        }
     }
    const pim = page.locator("//ul[@class='oxd-main-menu']/li//span[text()='PIM']");
    await pim.click();
--- tests/Locators/placeHolder.spec.ts
+++ tests/Locators/placeHolder.spec.ts
@@ -31,7 +31,7 @@ test.beforeEach("Login to OrangeHRM", async({page})=>{
     await page.getByPlaceholder("Password").fill("admin123");
     await page.waitForTimeout(3000);
     await page.getByRole('button', {name:' Login '}).click();
-    await page.waitForTimeout(3000);
+    await page.waitForURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
 });
 
 test.skip("Select the PIM",async({page})=>
@@ -63,19 +63,23 @@ test.skip("Validate the configuration", async({page}) =>{
     await page.waitForTimeout(3000);
 });
 
-test.skip("Validate the custom field in configuration list", async({page}) =>{
+test("Validate the custom field in configuration list", async({page}) =>{
 
     navigate_PIM(page);
     await page.getByText("Configuration ").click();
-    await page.waitForTimeout(3000);
+    // await page.waitForTimeout(3000);
+    await page.waitForSelector("//span[text()='Configuration ']/following-sibling::ul");
     // await page.pause();
     await page.getByRole("listitem").filter({hasText: /^Custom Fields$/}).click();
+    console.log("succesfully validate the custom filed under configuration list");
+    
 
 });
 
-test("validate the Employee List", async({page})=>{
+test.skip("validate the Employee List", async({page})=>{
     navigate_PIM(page);
     await page.waitForTimeout(2000);
+    await page.waitForSelector("//a[text()='Employee List']");
     await page.getByText("Employee List").click();
     await expect(page.url()).toContain("/viewEmployeeList");
 
@@ -88,10 +92,12 @@ test("validate the Employee List", async({page})=>{
     await page.waitForSelector("//label[text()='Job Title']/parent::div/following-sibling::div//div[@role='listbox']");
     const dd_listCount = page.locator("//label[text()='Job Title']/parent::div/following-sibling::div//div[@role='listbox']");
     console.log("Listbox count: "+await dd_listCount.count());
+    await page.waitForTimeout(3000);
     const dd_listElement =  page.locator("//label[text()='Job Title']/parent::div/following-sibling::div//div[@role='listbox']/div[@role='option']");
     console.log("Options count: "+await dd_listElement.count());
     const dd_options = await dd_listElement.allTextContents();
     console.log("all list of items: ",dd_options);
+    await page.pause();
     await dd_listElement.getByRole("option", {name: 'Chief Executive Officer'}).click();
     await page.waitForTimeout(3000)
     await expect(page.locator("//label[text()='Job Title']/parent::div/following-sibling::div//div[contains(text(), 'Chief Executive Officer')]")).toBeVisible();    
